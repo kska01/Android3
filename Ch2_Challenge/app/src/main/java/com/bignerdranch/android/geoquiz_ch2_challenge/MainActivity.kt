b2d16621c3e6adacc6ca9eebfa2e8a78472bc929
ka <insideka01@gmail.com>
2021-12-30 23:55:19 +0900

    12.30 ch2 challenge Button을 ImageButton으로 변경하기

--- Ch2_Challenge/app/src/main/java/com/bignerdranch/android/geoquiz_ch2_challenge/MainActivity.kt
+++ Ch2_Challenge/app/src/main/java/com/bignerdranch/android/geoquiz_ch2_challenge/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import com.bignerdranch.android.geoquiz.R
@@ -12,9 +13,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
-    private lateinit var previousButton: Button
+    private lateinit var previousButton: ImageButton
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
